5ce895ebe008cb241d10d2ced0422397cca4d169
Daniel Fahey <daniel.fahey@pm.me>
2024-02-13 13:14:25 +0000

    PyTorch 2.2.0

--- python-cuda12/flake.nix
+++ python-cuda12/flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Flake for a Python CUDA 12 development shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  # inputs.nixpkgs-torch.url = "github:daniel-fahey/nixpkgs/torch";
 
   nixConfig = {
     extra-substituters = [
@@ -16,50 +17,108 @@
     let
       system = "x86_64-linux";
 
+      # pkgs-torch = import nixpkgs-torch {
+      #   inherit system;
+      #   config = {
+      #     allowUnfree = true;
+      #     cudaSupport = true;
+      #   };
+      # };
+
+      # torchOverlay = final: prev: {
+      #   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+      #     (python-final: python-prev: {
+      #       torch = python-final.callPackage pkgs-torch.python311Packages.torch {};
+      #     })
+      #   ];
+      # };
+
       pkgs = import nixpkgs {
         inherit system;
+        # overlays = [ torchOverlay ];
         config = {
           allowUnfree = true;
           cudaSupport = true;
         };
       };
 
-      optimizedPython = pkgs.python3.override {
-        enableOptimizations = true;
-        reproducibleBuild = false;
-      };
+      # optimizedPython = pkgs.python311.override {
+      #   enableOptimizations = true;
+      #   reproducibleBuild = false;
+      # };
 
-      basicTools = with pkgs; [
-        optimizedPython
+      basicPkgs = with pkgs; [
+        # optimizedPython
+        python311
         poetry
         libGL
         glib
+        stdenv.cc.cc.lib
+        bashInteractive
+        zlib # needed by numpy
+        # ncurses5
+        # binutils
+        # # linuxPackages.nvidia_x11
+        # xorg.libXi
+        # xorg.libXmu
+        # freeglut
+        # xorg.libXext
+        # xorg.libX11
+        # xorg.libXv
+        # xorg.libXrandr
+        # libGLU
+        # gcc12 # for nvcc
       ];
 
-      gpuPackages = with pkgs.python3Packages; [
+      cudaPkgs = with pkgs.cudaPackages_12_1; [
+        # cudatoolkit
+        # cuda_cudart
+        # cuda_cupti
+        # cuda_nvcc
+        # cuda_nvrtc
+        # cuda_nvtx
+        # cudnn
+        # libcublas
+        # libcufft
+        # libcurand
+        # libcusparse
+        # nccl
+      ];
+
+      pythonPkgs = with pkgs.python311Packages; [
         torch
         torchvision
-        # opencv4
-        matplotlib
-        scikit-image
+        safetensors
+        psutil
+        einops
+        transformers
+        scipy
+        numpy
+        accelerate
+        piexif
+        xformers
       ];
 
-      devShellTools = basicTools ++ gpuPackages;
+      devShellPkgs = basicPkgs ++ cudaPkgs ++ pythonPkgs;
 
     in {
-      devShell.x86_64-linux = pkgs.mkShell {
+      
+      devShell.${system} = pkgs.mkShell {
         
         hardeningDisable = ["all"];
 
-        buildInputs = devShellTools;
+        buildInputs = devShellPkgs;
 
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath devShellTools;
-        # LD_LIBRARY_PATH = "${with pkgs; lib.makeLibraryPath devShellTools}:/run/opengl-driver/lib";
+        PATH = pkgs.lib.makeBinPath devShellPkgs;
 
-        POETRY_VIRTUALENVS_IN_PROJECT = "true";
-        POETRY_VIRTUALENVS_PATH = "{project-dir}/.venv";
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath devShellPkgs;
 
-        POETRY_VIRTUALENVS_PREFER_ACTIVE_PYTHON = "true";
+        # POETRY_VIRTUALENVS_IN_PROJECT = true;
+        # POETRY_VIRTUALENVS_OPTIONS_NO_PIP = true;
+        # POETRY_VIRTUALENVS_PATH = "{project-dir}/.venv";
+        # POETRY_VIRTUALENVS_OPTIONS_SYSTEM_SITE_PACKAGES = true;
+
+        # POETRY_VIRTUALENVS_PREFER_ACTIVE_PYTHON = true;
 
       };
     };
